b974cb33eccc9e99a029158fed397dae4eb11f0d
The Xires <xires.dev@gmail.com>
2022-05-04 14:33:44 +0200

    added back button to project page

--- components/project/ProjectHeader.tsx
+++ components/project/ProjectHeader.tsx
@@ -1,4 +1,6 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
 import styles from '../../styles/project/projectHeader.module.css';
 import TechnologyIcon, { IconName } from '../general/TechnologyIcon';
 
@@ -9,9 +11,15 @@ interface Props {
 }
 
 function ProjectHeader({ imgUrl, heading, icons }: Props) {
+  const router = useRouter();
+
   return (
     <section className={styles.container}>
-      <div className={styles.image} style={{ backgroundImage: `url(${imgUrl})` }} />
+      <div className={styles.image} style={{ backgroundImage: `url(${imgUrl})` }}>
+        <div className={styles.backButton} onClick={() => router.back()}>
+          <FiArrowLeft size={26} />
+        </div>
+      </div>
       <div className={styles.bottom}>
         <div className={styles.heading}>{heading}</div>
         <div className={styles.iconContainer}>
